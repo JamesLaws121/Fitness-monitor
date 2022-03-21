4a3168fdec1fd6b043c8abd40ca8ac5ad01e5c03
jla201 <jla201@uclive.ac.nz>
2022-03-22 11:44:53 +1300

    Implemented circular buffer

--- circBufT.c
+++ circBufT.c
@@ -12,20 +12,19 @@
 #include <stdint.h>
 #include "stdlib.h"
 #include "circBufT.h"
+#include "acc.h"
 
 // *******************************************************
 // initCircBuf: Initialise the circBuf instance. Reset both indices to
 // the start of the buffer.  Dynamically allocate and clear the the 
 // memory and return a pointer for the data.  Return NULL if 
 // allocation fails.
-uint32_t *
-initCircBuf (circBuf_t *buffer, uint32_t size)
+vector3_t * initCircBuf (circBuf_t *buffer, uint32_t size)
 {
 	buffer->windex = 0;
 	buffer->rindex = 0;
 	buffer->size = size;
-	buffer->data = 
-        (uint32_t *) calloc (size, sizeof(uint32_t));
+	buffer->data =  (vector3_t *) calloc (size, sizeof(vector3_t));
 	return buffer->data;
 }
    // Note use of calloc() to clear contents.
@@ -33,8 +32,7 @@ initCircBuf (circBuf_t *buffer, uint32_t size)
 // *******************************************************
 // writeCircBuf: insert entry at the current windex location,
 // advance windex, modulo (buffer size).
-void
-writeCircBuf (circBuf_t *buffer, uint32_t entry)
+void writeCircBuf (circBuf_t *buffer, vector3_t entry)
 {
 	buffer->data[buffer->windex] = entry;
 	buffer->windex++;
@@ -46,10 +44,9 @@ writeCircBuf (circBuf_t *buffer, uint32_t entry)
 // readCircBuf: return entry at the current rindex location,
 // advance rindex, modulo (buffer size). The function deos not check
 // if reading has advanced ahead of writing.
-uint32_t
-readCircBuf (circBuf_t *buffer)
+vector3_t readCircBuf (circBuf_t *buffer)
 {
-	uint32_t entry;
+    vector3_t entry;
 	
 	entry = buffer->data[buffer->rindex];
 	buffer->rindex++;
