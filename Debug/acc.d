0d25e909b33ec0b4ef62a59d8a40489418d57edb
jla201 <jla201@uclive.ac.nz>
2022-03-21 13:46:26 +1300

    minor fixes

---
+++ Debug/acc.d
@@ -0,0 +1,90 @@
+# FIXED
+
+acc.obj: ../acc.c
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdlib.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h
+acc.obj: ../../OrbitOLED/OrbitOLEDInterface.h
+acc.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdarg.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/time.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/abi_prefix.h
+acc.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_time.h
+acc.obj: ../acc.h
+acc.obj: ../i2c_driver.h
+
+../acc.c:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdlib.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h:
+
+../../OrbitOLED/OrbitOLEDInterface.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdarg.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/time.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/abi_prefix.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_time.h:
+
+../acc.h:
+
+../i2c_driver.h:
+
